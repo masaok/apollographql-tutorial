54b55aa5188619d9568b7997b08415328613be12
Masao Kitamura <masaok@gmail.com>
2020-03-13 07:05:52 -0700

    Added data sources to Apollo Server

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,22 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema');
+const { ApolloServer } = require('apollo-server')
+const typeDefs = require('./schema')
 
-const server = new ApolloServer({ typeDefs });
+// https://www.apollographql.com/docs/tutorial/data-source/#add-data-sources-to-apollo-server
+const { createStore } = require('./utils')
+
+const LaunchAPI = require('./datasources/launch')
+const UserAPI = require('./datasources/user')
+
+const store = createStore()
+
+const server = new ApolloServer({
+  typeDefs,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store })
+  })
+})
 
 server.listen().then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
-  });
+  console.log(`🚀 Server ready at ${url}`)
+})
